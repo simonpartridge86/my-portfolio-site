c9fc9b4ae3b86dd6ceab71407ff9d4287fc7da30
simonpartridge86 <simonpartridge86@gmail.com>
2022-07-15 00:47:34 +0100

    added portfolio and skills section placeholders

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import { NavBar } from "./NavBar";
+import { IntroSection } from './Components/IntroSection';
+import { NavBar } from "./Components/NavBar";
+import { PortfolioSection } from './Components/PortfolioSection';
 
 function App() {
   return (
     <>
       <NavBar/>
-      <div className = "intro-section">
-        <img src="https://i.postimg.cc/d18jz1NR/avatar-1024.png" alt="pixel avatar"></img>
-        <div className = "intro-text">
-          <h1>Hi, I'm Simon Partridge</h1>
-          <br/>
-          <p>I’m a <span className="accent-text">Doctor of Chemistry</span> and former copy editor.</p>
-          <br/>
-          <p>I travelled the globe over the last 10 years, but now I’m starting a new adventure as a <span className="accent-text">Junior Software Developer</span>.</p>
-        </div> 
+      <IntroSection />
+      <PortfolioSection />
+      <div className="skills-section">
+        <h1>My Skillset</h1>
+        <img className="tech-image" src="https://i.postimg.cc/xCVhMfBC/soc-technologies.png" alt="my tech" />
       </div>
+      <img
+          className="pixel-bar-top"
+          src="https://i.postimg.cc/CLY9rxPp/Screenshot-2022-07-14-at-23-38-50.png"
+          alt="pixel bar green"
+      />
     </>
   );
 }
---
+++ src/Components/ProjectCard.js
@@ -0,0 +1,9 @@
+export function ProjectCard() {
+    return (
+        <div className="project-card">
+            <img src="https://i.postimg.cc/d18jz1NR/avatar-1024.png" alt="pixel avatar"></img>
+            <h2>Project Title</h2>
+            <p>Some info about the project will go here. Some info about the project will go here. Some info about the project will go here.</p>
+        </div>
+    )
+}
