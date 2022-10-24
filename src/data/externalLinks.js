e6bf15e4a4f021f402a4712baed4eca522d2d48c
simonpartridge86 <simonpartridge86@gmail.com>
2022-10-24 19:51:58 +0100

    cv link updated

--- src/data/externalLinks.js
+++ src/data/externalLinks.js
@@ -1,5 +1,5 @@
 const externalLinks = {
-  cv: "https://www.canva.com/design/DAFO2NxMh58/x2-7Hc_ruQk32GOflp0sMg/view?utm_content=DAFO2NxMh58&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink",
+  cv: "https://www.canva.com/design/DAFP8r1wV4I/VRkDLn_mPB6cOYipJZhy_A/view?utm_content=DAFP8r1wV4I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
   github: "https://github.com/simonpartridge86",
   email: "mailto:simonpartridge86@gmail.com",
   linkedin: "https://www.linkedin.com/in/simonpartridge86/",
