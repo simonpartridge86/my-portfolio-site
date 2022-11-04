77443fe06a1eb7bd6b63225b5ab093b20e1dbdd7
simonpartridge86 <simonpartridge86@gmail.com>
2022-11-04 09:44:09 +0000

    pixel transition spacing updated

--- src/components/PixelTransition.js
+++ src/components/PixelTransition.js
@@ -6,11 +6,11 @@ export default function PixelTransition({ src }) {
 
 const Image = styled.img`
   width: 100%;
-  height: 200px;
+  height: 100px;
   object-fit: cover;
   padding: 0;
-  margin-top: -25px;
-  margin-bottom: -100px;
+  margin-top: -20px;
+  margin-bottom: -20px;
   box-sizing: border-box;
   border-right: 6px solid var(--dark-color);
   border-left: 6px solid var(--dark-color);
