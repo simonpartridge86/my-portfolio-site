1aaca8b841e8145f6d115aa0363c215e42149c05
simonpartridge86 <simonpartridge86@gmail.com>
2022-12-19 17:56:48 +0000

    changed fetch to graphql query

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,15 +1,31 @@
-import useFetch from "../hooks/useFetch";
+//import useFetch from "../hooks/useFetch";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const BLOGS = gql`
+  query GetBlogs {
+    blogs {
+      data {
+        id
+        attributes {
+          title
+          body
+        }
+      }
+    }
+  }
+`;
 
 export default function Blog() {
-  const { loading, error, data } = useFetch("http://localhost:1337/api/blogs");
+  //const { loading, error, data } = useFetch("http://localhost:1337/api/blogs");
+  const { loading, error, data } = useQuery(BLOGS);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error!</p>;
   console.log(data);
   return (
     <div>
-      {data.map((post) => (
+      {data.blogs.data.map((post) => (
         <BlogSection key={post.id} className="blogpost">
           <h2 className="title">{post.attributes.title}</h2>
           <p className="body">{post.attributes.body.substring(0, 200)}...</p>
